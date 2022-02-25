951b27c18ec72fd9667e8de123c4e3cd9699836a
Deepak Chopra <88072239+DeepakChopra0@users.noreply.github.com>
2022-02-25 12:20:38 +0530

    25 Feb 2022

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            user: config.user,
-            password: config.password,
+            host: "ec2-44-193-188-118.compute-1.amazonaws.com",//config.host,
+            port: "5432", //config.port,
+            database: "dc3950i2bmrnnc", //config.database,
+            user: "fzwijulchtfgvs",  //config.user,
+            password: "279f7f484e6d8c7a691cb2cb091efcc2ceec8b96da338da27104aa50a82de5ff", // config.password,
             ssl: {
                 rejectUnauthorized: false
             },
